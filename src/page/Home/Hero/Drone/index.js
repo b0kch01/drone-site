d8e2c1b5716ba9de8798d536a8682c93f057f052
Nathan Choi <nath49ers@gmail.com>
2025-09-05 11:58:19 -0700

    drone footage

---
+++ src/page/Home/Hero/Drone/index.js
@@ -0,0 +1,60 @@
+"use client";
+
+import { motion, useMotionValueEvent, useTransform } from "motion/react";
+import { useEffect, useRef } from "react";
+
+const N_FRAMES = 122;
+
+const getFrame = (frame) =>
+  `/drone_frames/drone_sweep${frame.toString().padStart(3, "0")}.jpg`;
+
+export function Drone({ scrollY }) {
+  const frames = useRef([]);
+  const context = useRef(null);
+
+  const scale = useTransform(scrollY, [0, 1], [0.6, 1]);
+  const positionY = useTransform(scrollY, [0, 1], ["20vh", "0vh"]);
+
+  useMotionValueEvent(scrollY, "change", (latestValue) => {
+    console.log(latestValue);
+    const index = Math.max(
+      0,
+      Math.min(Math.ceil(N_FRAMES * latestValue), N_FRAMES - 1),
+    );
+    context.current?.drawImage(frames.current[index], 0, 0);
+  });
+
+  useEffect(() => {
+    console.log("Preloading images");
+    for (let i = 0; i < N_FRAMES; ++i) {
+      const img = new Image();
+      img.src = getFrame(i);
+      frames.current.push(img);
+    }
+
+    const canvas = document.getElementById("drone");
+    context.current = canvas.getContext("2d");
+
+    canvas.width = 1504;
+    canvas.height = 846;
+
+    frames.current[0].onload = () => {
+      context.current.drawImage(frames.current[0], 0, 0);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      initial={{ scale: 0.8, y: "-5vh", filter: "blur(10px)", opacity: 0 }}
+      animate={{ scale: 1, y: 0, filter: "blur(0px)", opacity: 1 }}
+      transition={{ type: "spring", visualDuration: 4, bounce: 0, delay: 2 }}
+      className="sticky left-0 top-0 h-screen w-full flex items-center justify-center"
+    >
+      <motion.canvas
+        style={{ scale, y: positionY }}
+        className="w-full"
+        id="drone"
+      />
+    </motion.div>
+  );
+}
--- src/page/Home/Hero/index.js
+++ src/page/Home/Hero/index.js
@@ -1,14 +1,28 @@
 "use client";
 
-import { motion, useSpring, useTransform } from "motion/react";
-import { memo } from "react";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import { memo, useRef } from "react";
 
 import { TextEffect } from "@/components/motion-primitives/text-effect";
 import { TextScramble } from "@/components/motion-primitives/text-scramble";
 
+import { Drone } from "./Drone";
+
 const R = 20;
 
 export const Hero = memo(function Hero({ mouse }) {
+  const hero = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: hero,
+    offset: ["start start", "end end"],
+  });
+
+  const smoothScroll = useSpring(scrollYProgress, {
+    visualDuration: 0.4,
+    bounce: 0,
+  });
+
   const mouseX = useSpring(mouse.x);
   const mouseY = useSpring(mouse.y);
 
@@ -17,35 +31,57 @@ export const Hero = memo(function Hero({ mouse }) {
   const positionXHeavy = useTransform(mouseX, [0, 1], [-R / 2, R / 2]);
   const positionYHeavy = useTransform(mouseY, [0, 1], [-R / 2, R / 2]);
 
-  return (
-    <div className="border-red-500 border-2 min-h-screen flex flex-col items-center justify-center">
-      <motion.h1 className="select-none flex flex-col text-center">
-        <TextEffect
-          style={{ x: positionX, y: positionY }}
-          className="font-gleam text-3xl font-light tracking-tighter"
-          preset="fade-in-blur"
-          delay={1}
-          speedReveal={0.5}
-          speedSegment={0.4}
-        >
-          Tap Into Modern
-        </TextEffect>
+  const scrollYOffset = useTransform(scrollYProgress, [0, 1], ["0", "80vh"]);
+  const scrollGap = useTransform(smoothScroll, [0, 1], ["0", "10vh"]);
+  const scrollScale = useTransform(smoothScroll, [0, 1], [1, 0.8]);
+  const scrollBlur = useTransform(
+    smoothScroll,
+    [0, 1],
+    ["blur(0px)", "blur(10px)"],
+  );
 
-        <motion.div
-          initial={{ opacity: 0, y: 50, scale: 1.2, filter: "blur(10px)" }}
-          animate={{ opacity: 1, y: 0, scale: 1, filter: "blur(0px)" }}
-          transition={{ duration: 2, delay: 2 }}
+  return (
+    <div
+      ref={hero}
+      className="border-red-500 border-2 min-h-[200vh] flex flex-col items-center"
+    >
+      <Drone scrollY={smoothScroll} />
+
+      <motion.div
+        style={{ y: scrollYOffset, scale: scrollScale, filter: scrollBlur }}
+        className="absolute top-0 min-h-screen flex items-center"
+      >
+        <motion.h1
+          style={{ gap: scrollGap }}
+          className="select-none flex flex-col text-center"
         >
-          <TextScramble
-            style={{ x: positionXHeavy, y: positionYHeavy }}
-            className="font-gleam text-9xl font-mono"
-            duration={4}
+          <TextEffect
+            style={{ x: positionX, y: positionY }}
+            className="font-gleam text-3xl font-light tracking-tighter"
+            preset="fade-in-blur"
             delay={1}
+            speedReveal={0.5}
+            speedSegment={0.4}
           >
-            Surveillance
-          </TextScramble>
-        </motion.div>
-      </motion.h1>
+            Tap Into Modern
+          </TextEffect>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50, scale: 1.2, filter: "blur(10px)" }}
+            animate={{ opacity: 1, y: 0, scale: 1, filter: "blur(0px)" }}
+            transition={{ duration: 2, delay: 2 }}
+          >
+            <TextScramble
+              style={{ x: positionXHeavy, y: positionYHeavy }}
+              className="font-gleam font-mono text-[10vw] leading-[10vw] uppercase px-2 tracking-tighter"
+              duration={4}
+              delay={1}
+            >
+              Surveillance
+            </TextScramble>
+          </motion.div>
+        </motion.h1>
+      </motion.div>
     </div>
   );
 });
